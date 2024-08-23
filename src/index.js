568b2f258a9ce7f7f757679b5a824a010dee8579
AbirHossen <abirhossen16@gmail.com>
2024-08-23 16:59:31 +0600

    Passing and Receiving props

--- src/index.js
+++ src/index.js
@@ -69,14 +69,34 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, Mushrooms"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
   );
 }
-
+function Pizza(props) {
+  return (
+    <div class="pizza">
+      <img src={props.photoName} alt={props.name}></img>
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
+  );
+}
 function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
@@ -95,16 +115,6 @@ function Footer() {
     <footer className="footer">
       {new Date().toLocaleTimeString()}.We're currently open!
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci"></img>
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
   );
 }
 
